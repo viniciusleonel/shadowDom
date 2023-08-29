3460b879edbdf41e6b3c68eb2b79a499838b0c70
Vinicius Leonel <viniciuslps.cms@gmail.com>
2023-08-29 18:14:36 -0300

    Aprendendo sobre Shadow Dom

---
+++ src/components/tituloPorComponent.js
@@ -0,0 +1,13 @@
+class titutloPorComponent extends HTMLElement {
+    constructor(){
+        super();
+
+        // Criando um component fixo no Html
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.innerHTML = `
+        <h1 class="teste">Component Fixo HTML</h1>
+        `
+    }
+}
+
+customElements.define("titulo-fixo", titutloPorComponent);
